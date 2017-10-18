4e057c5cb7827e327c7e605853a7b41d1013e7cf
matiheimann <mheimann@itba.edu.ar>
2017-10-18 18:28:48 -0300

    super commit

---
+++ tp/RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/sampleCodeModule.c
@@ -0,0 +1,25 @@
+/* sampleCodeModule.c */
+
+char * v = (char*)0xB8000;
+
+static int var1 = 0;
+static int var2 = 0;
+
+
+int main() {
+	//All the following code may be removed 
+	char * s = "Arquitectura de las computadoras";
+	while(*s){
+		*v = *s;
+		v++;
+		*v = 0;
+		v++;
+		s++;
+	}
+
+	//Test if BSS is properly set up
+	if (var1 == 0 && var2 == 0)
+		return 0xDEADC0DE;
+
+	return 0xDEADBEEF;
+}
