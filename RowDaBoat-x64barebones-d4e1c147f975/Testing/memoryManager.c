4b924de4f8818020bd30bd7bda2c231060dcec3c
matiheimann <mheimann@itba.edu.ar>
2018-04-20 04:33:46 -0300

    Add files via upload

--- RowDaBoat-x64barebones-d4e1c147f975/Testing/memoryManager.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Testing/memoryManager.c
@@ -68,9 +68,99 @@ void* alloc(size_t bytes)
 
 }
 
+void dealloc(void* address)
+{
+    if(address <= (void*)&memory || (void*)(address > &memory + 4194304))
+    {
+        return;
+    }
+
+    int position = address - (void*)&memory - 1;
+
+    if(position % 4096 != 0 || memory[position] % 2 == 0)
+    {
+        return;
+    }
+
+    unsigned char c1 = 0;
+    unsigned char c2 = 0;
+    metadata * metadata1 = (&c1, 0, 1);
+    metadata * metadata2 = (&c2, 0, 1);
+
+    memory[position] -= 1;
+
+    while(position >= 0 && position < 4194304)
+    {
+        readMetadata(position, metadata1);
+        if(metadata1->isLeft)
+        {
+            int positionBuddy = jumpNextPosition(position, metadata1->order);
+
+            if(positionBuddy < 0 || positionBuddy >= 4194304)
+            {
+                return;
+            }
+
+            readMetadata(positionBuddy, metadata2);
+
+            if(metadata2->isAllocated || metadata1->order != metadata2->order)
+            {
+                return;
+            }
+
+            mergeBlocks(position, positionBuddy, metadata1->order);
+
+        }
+        else
+        {
+            int positionBuddy = jumpPreviousPosition(position, metadata1->order);
+
+            if(positionBuddy < 0 || positionBuddy >= 4194304)
+            {
+                return;
+            }
+
+            readMetadata(positionBuddy, metadata2);
+
+            if(metadata2->isAllocated || metadata1->order != metadata2->order)
+            {
+                return;
+            }
+
+            mergeBlocks(positionBuddy, position, metadata1->order);
+        }
+    }
+
+}
+
+void mergeBlocks(int leftPosition, int rightPosition, int order)
+{
+    char newMetadata = 4 * (order + 1);
+
+    if(isLeft(position, order))
+    {
+        newMetadata += 2;
+    }
+
+    memory[rightPosition] = 0;
+
+    memory[leftPosition] = newMetadata;
+}
+
+int isLeft(int position, int order)
+{
+    return (position % pow(2, 13 + order) == 0) ? 1 : 0;
+}
+
 int jumpNextPosition(int position, int order)
 {
     position += (int)(pow(2, order + 12));
+    return position;
+}
+
+int jumpPreviousPosition(int position, int order)
+{
+    position -= (int)(pow(2, order + 12));
     return position;
 }
 
